c12c7f4f13f3623a25c5a671b0f3cc0ccdfd8783
Guilherme Martins <guilherme_790@hotmail.com>
2022-06-01 21:33:55 -0300

    Finalizando aula sobre tuplas

--- guppe/Sessao 7 - Colecoes Phyton/Tuplas.py
+++ guppe/Sessao 7 - Colecoes Phyton/Tuplas.py
@@ -1,13 +1,16 @@
 """
 Tuplas (tuple)
 
-Tuplas são parecidas com listas
+Tuplas são parecidas com listas, mas são mais rápidas. Tuplas deixam o código mais seguro,
+pois os elementos são imutáveis.
 
 Existem duas diferenças básicas
 
 1- As tuplas são representadas por parenteses ()
 2- As tuplas são imutáveis. Ao criar uma tupla, ela não muda. Toda operação em
 uma tupla gera uma nova tupla.
+
+# Métodos para adição e remoção de elementos nas tuplas não existem, dado o fato das tuplas serem imutáveis.
 
 ----------------------------------------------------------------------
 # Cuidado1: As tuplas são representadas por (), mas veja
@@ -46,19 +49,61 @@ print(escola)
 print(curso)
 
 ----------------------------------------------------------------------
+tupla = (1, 2, 3, 4, 5, 6)
 
+print(sum(tupla)) #Soma valores tupla
+print(max(tupla))
+print(min(tupla))
+print(len(tupla))
 ----------------------------------------------------------------------
+# Concatenação de tuplas
 
+tupla1 = (1, 2, 3)
+print(tupla1)
+
+tupla2 = (4, 5, 6)
+print(tupla2)
+
+print(tupla1 + tupla2)
+
+tupla3 = tupla1 + tupla2
+
+print(tupla3)
+
+tupla1 = tupla1 + tupla2
+
+print(tupla1)
 ----------------------------------------------------------------------
+# Verificar se determinado elemento está contido na tupla
 
+tupla = (1, 2, 3)
+
+print(3 in tupla)
+print(34 in tupla)
 ----------------------------------------------------------------------
+# Iterando sobre uma tupla
 
+tupla = (1, 2, 3, 4, 5)
+
+for n in tupla:
+    print(n)
+
+print("Indice valor")
+for indice, valor in enumerate(tupla):
+    print(indice, valor)
 ----------------------------------------------------------------------
+# Contando elementos dentro de uma tupla
 
+tupla = ('a', 'b', 'c', 'd', 'd', 'd', 'e', 'f')
+
+print(tupla.count('d'))
 ----------------------------------------------------------------------
-
+# Dicas na utilização de tuplas
+# Devemos utilizar tuplas sempre que não precisarmos modificar dados contidus numa coleção
+# EX
+meses = ('Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez')
 ----------------------------------------------------------------------
-
 """
+
 
 
